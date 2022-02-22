9262f5bc0933542fce26b5436ba185206b0632d1
Alexandre R. Saccol <arsaccol@gmail.com>
2022-02-22 16:26:24 -0300

    Remove onDelete cascade thingies from models

--- server/src/models.js
+++ server/src/models.js
@@ -51,8 +51,8 @@ const Post = sequelize.define(
 
 
 // one user to many posts
-User.hasMany(Post, {foreignKey: {allowNull: false}, onDelete: 'CASCADE'})
-Post.belongsTo(User, {foreignKey: {allowNull: false}, onDelete: 'CASCADE'})
+User.hasMany(Post, {foreignKey: {allowNull: false}})
+Post.belongsTo(User, {foreignKey: {allowNull: false}})
 
 await (async () => {
     sequelize.sync()

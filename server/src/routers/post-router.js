d5d0ef59cc05cc40c2562c7e989019c448baf27c
Alexandre R. Saccol <arsaccol@gmail.com>
2022-02-23 15:43:48 -0300

    Implement routes and respective handlers for user posts

--- server/src/routers/post-router.js
+++ server/src/routers/post-router.js
@@ -1,16 +1,50 @@
 import express from "express"
-import { Post } from '../models.js'
+import { Post, User } from '../models.js'
 
 const router = express.Router()
 
-router.get('/posts/', async (req, res) => {
+router.get('/users/:id/posts/', async (req, res) => {
     try {
-        const results = await Post.findAll()
-            res.json(results)
+        const results = await Post.findAll({
+            where: {
+                userId: req.params.id
+            }
+        })
+        res.json(results)
     }
     catch(err) {
-        console.log(`Error retrieving posts`)
+        console.log(`Error retrieving posts: ${err}`)
         res.send(500, "Error retrieving posts")
+    }
+})
+
+
+router.post('/users/:id/posts/', async(req, res) => {
+    try {
+        const { id } = req.params
+        const user = (await User.findOne({ where: { id: id } }))
+
+        const { content } = req.body
+
+        console.log(`CREATING POST FOR USER "${id}" WITH CONTENT "${content}"`)
+
+        const postTime = Date.now()
+
+        if(content) {
+            const createdPost = user.createPost({
+                postTime: postTime,
+                content: content
+            })
+
+            console.log({id: createdPost.id, content: createdPost.content, postTime: createdPost.postTime})
+
+            res.json(createdPost)
+        }
+
+    }
+    catch(err) {
+        console.log(`Error creating post: "${err}"`)
+        res.send(500, "Error creating post")
     }
 })
 

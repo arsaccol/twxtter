6227fde0fbd95312255aa5cc1039a1a67571713d
Alexandre R. Saccol <arsaccol@gmail.com>
2022-03-05 08:44:55 -0300

    Move effect hook async functions into synchronous hook callback, per warning from react scripts

--- client/src/hooks/fetchUserProfile.js
+++ client/src/hooks/fetchUserProfile.js
@@ -5,18 +5,23 @@ import axios from 'axios'
 function useUserProfile(userId) {
     const [userProfile, setUserProfile] = useState({fetchStatus: "fetching"})
 
-    useEffect(async () => {
-        try {
-            const user = await axios.get(`http://localhost:3000/users/${userId}`)
-            if(user.data)
-                setUserProfile({...user.data, fetchStatus: "done"})
-            else
-                setUserProfile({fetchStatus: "not-found"})
+    useEffect(() => {
+        async function fetchUserProfile() 
+        {
+            try {
+                const user = await axios.get(`http://localhost:3000/users/${userId}`)
+                if(user.data)
+                    setUserProfile({...user.data, fetchStatus: "done"})
+                else
+                    setUserProfile({fetchStatus: "not-found"})
+            }
+            catch(err) {
+                console.log(`Error fetching user: ${err}`)
+                return {fetchStatus: "error"}
+            }
         }
-        catch(err) {
-            console.log(`Error fetching user: ${err}`)
-            return {fetchStatus: "error"}
-        }
+
+        fetchUserProfile()
     }, [userId])
 
     return userProfile
--- client/src/hooks/useUserPosts.js
+++ client/src/hooks/useUserPosts.js
@@ -4,21 +4,24 @@ import axios from 'axios'
 function useUserPosts(userId) {
     const [userPosts, setUserPosts] = useState({posts: [], fetchStatus: "fetching"})
 
-    useEffect(async() => {
+    useEffect(() => {
+        async function fetchUserPosts()
+        {
+            try {
+                const response = await axios.get(`http://localhost:3000/users/${userId}/posts`)
 
-        try {
-            const response = await axios.get(`http://localhost:3000/users/${userId}/posts`)
-
-            if(response.data.length > 0)
-                setUserPosts({posts: response.data, fetchStatus: "fetched"})
-            else
-                setUserPosts({posts: [], fetchStatus: "not-found"})
-        } 
-        catch (err) {
-            console.log(`Error fetching posts of user ${userId}: ${err}`)
-            return { fetchStatus: "error" }
+                if(response.data.length > 0)
+                    setUserPosts({posts: response.data, fetchStatus: "fetched"})
+                else
+                    setUserPosts({posts: [], fetchStatus: "not-found"})
+            } 
+            catch (err) {
+                console.log(`Error fetching posts of user ${userId}: ${err}`)
+                return { fetchStatus: "error" }
+            }
         }
 
+        fetchUserPosts()
     }, [userId])
 
     return userPosts

9f2c32e318c14912adb958ebda52dd207a14de2c
Alexandre R. Saccol <arsaccol@gmail.com>
2022-03-11 04:26:07 -0300

    Add incomplete login method to auth context

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -2,9 +2,13 @@ import { createContext } from 'react'
 
 const AuthContext = createContext({})
 
+async function Login() {
+    console.log(`Logged in!`)
+}
+
 export function AuthProvider({children}) {
 
-    return <AuthContext.Provider value = {{ signedIn: false, user: {id: 0, username: null}}}>
+    return <AuthContext.Provider value = {{ signedIn: false, user: {id: 0, username: null}, Login}}>
         { children }
 
     </AuthContext.Provider>
